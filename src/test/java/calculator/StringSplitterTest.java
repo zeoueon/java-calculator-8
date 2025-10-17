28f419f71a9d0825de6c634a9fa1b64b69bcd7a5
zeoueon <zeoueon129@gmail.com>
2025-10-17 21:32:13 +0900

    test: StringSplitter 테스트 구현
    주어진 구분자로 입력을 정확히 나누는지 테스트한다.

---
+++ src/test/java/calculator/StringSplitterTest.java
@@ -0,0 +1,21 @@
+package calculator;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import calculator.model.StringSplitter;
+import org.junit.jupiter.api.Test;
+
+public class StringSplitterTest {
+
+    @Test
+    public void test() {
+        //given
+        StringSplitter stringSplitter = new StringSplitter("//;\\n1");
+
+        //when
+        String result[] = stringSplitter.splitString(";");
+
+        //then
+        assertThat(result).containsExactly("1");
+    }
+}
