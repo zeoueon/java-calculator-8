2f3ed16e6c72fd52d894ebf6a72125385cc1fdd3
zeoueon <zeoueon129@gmail.com>
2025-10-17 15:46:44 +0900

    fix: 출력 데이터 타입 수정
    합계의 데이터 타입을 Integer -> Double 로 수정한다.

--- src/main/java/calculator/view/Output.java
+++ src/main/java/calculator/view/Output.java
@@ -2,7 +2,7 @@ package calculator.view;
 
 public class Output {
 
-    public void printSum(int sum) {
-        System.out.println("결과: " + sum);
+    public void printSum(Double sum) {
+        System.out.println("결과 : " + sum);
     }
 }
