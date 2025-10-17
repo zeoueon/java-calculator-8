bc13aa742f0604f5430b0754838e9efac5305a77
zeoueon <zeoueon129@gmail.com>
2025-10-17 15:16:55 +0900

    fix: 구분자 추출 인ãì오류 수정
    ㅇ구분자 추출 인덱스 오류를 수정한다.

--- src/main/java/calculator/model/DelimiterExtractor.java
+++ src/main/java/calculator/model/DelimiterExtractor.java
@@ -6,17 +6,16 @@ public class DelimiterExtractor {
     private final static String FRONT_DELIMITER = "//";
     private final static String BACK_DELIMITER = "\\n";
 
-    public String extractDelimiter(String inputString) { // 일단 여기서는 검증이 된 입력이라고 가정하자.
-        if (!(inputString.substring(0, 2).equals(FRONT_DELIMITER))) {
+    public String extractDelimiter(String inputString) {
+        if (!(inputString.startsWith(FRONT_DELIMITER))) {
             return DEFAULT_DELIMITER;
         }
 
         int delimiterLastIndex = inputString.indexOf(BACK_DELIMITER);
         if (delimiterLastIndex == -1) {
-            throw new IllegalArgumentException("올바르지 않은 입력 형식입니다.");
+            throw new IllegalArgumentException("커스텀 구분자를 올바른 형식으로 설정하세요.");
         }
-        
-        return inputString.substring(0, delimiterLastIndex);
-    }
 
+        return inputString.substring(2, delimiterLastIndex);
+    }
 }
