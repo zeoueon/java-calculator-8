21109ae7868cd51e2969bc3ed664b7cd3e868651
zeoueon <zeoueon129@gmail.com>
2025-10-18 21:29:27 +0900

    feat: 오버플로우 방지 로직 구현
    오버플로우가 발생할시 IllegalArgumentException로 처리한다.

--- src/main/java/calculator/model/SumCalculator.java
+++ src/main/java/calculator/model/SumCalculator.java
@@ -14,6 +14,9 @@ public class SumCalculator implements Calculator {
     }
 
     public Double formatDouble(Double number) {
+        if (number > Double.MAX_VALUE) {
+            throw new IllegalArgumentException("숫자가 너무 큽니다.");
+        }
         return Math.round(number * 1000) / 1000.0; // 소수점 셋째자리까지 표시
     }
 }
