05296064c90670dc246f0b12982364333eafe88b
zeoueon <zeoueon129@gmail.com>
2025-10-18 20:58:44 +0900

    refactor: converter 호출 로직 추가
    StringToNumbersConverter를 생성 및 호출한다.
    SumCalcuator를 호출한다.

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,29 +1,30 @@
 package calculator.controller;
 
-import calculator.model.Calculator;
-import calculator.model.DelimiterExtractor;
+import calculator.model.StringToNumbersConverter;
+import calculator.model.SumCalculator;
 import calculator.view.Input;
 import calculator.view.Output;
+import java.util.List;
 
 public class CalculatorController {
 
     private Input input;
     private Output output;
-    private Calculator calculator;
-    private DelimiterExtractor delimiterExtractor;
+    private SumCalculator calculator;
+    private StringToNumbersConverter stringToNumbersConverter;
 
     public CalculatorController() {
         input = new Input();
         output = new Output();
-        calculator = new Calculator();
-        delimiterExtractor = new DelimiterExtractor();
+        calculator = new SumCalculator();
+        stringToNumbersConverter = new StringToNumbersConverter();
     }
 
     public void run() {
         String inputString = input.inputString();
 
-        String delimiter = delimiterExtractor.extractDelimiter(inputString);
-        Double sum = calculator.calculate(inputString, delimiter);
+        List<Double> numberList = stringToNumbersConverter.convertToNumbers(inputString);
+        Double sum = calculator.calculate(numberList);
 
         output.printSum(sum);
     }
