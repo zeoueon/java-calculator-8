1596c815f5bc2c1bc759957350ee5ef35f98b413
zeoueon <zeoueon129@gmail.com>
2025-10-18 21:54:52 +0900

    feat: 디폴트 구분자 예외 처리 추가
    디폴트 구분자를 커스텀 구분자 형식으로 선언할 수 없도록 예외를 추가한다.

--- src/main/java/calculator/model/DelimiterExtractor.java
+++ src/main/java/calculator/model/DelimiterExtractor.java
@@ -12,10 +12,23 @@ public class DelimiterExtractor {
         }
 
         int delimiterLastIndex = inputString.indexOf(BACK_DELIMITER);
+        validateDelimiter(delimiterLastIndex);
+
+        String delimiter = inputString.substring(2, delimiterLastIndex);
+        validateDelimiterNotDefault(delimiter);
+
+        return delimiter;
+    }
+
+    private void validateDelimiter(int delimiterLastIndex) {
         if (delimiterLastIndex == -1) {
             throw new IllegalArgumentException("커스텀 구분자를 올바른 형식으로 설정하세요.");
         }
+    }
 
-        return inputString.substring(2, delimiterLastIndex);
+    private void validateDelimiterNotDefault(String delimiter) {
+        if (delimiter.matches(DEFAULT_DELIMITER)) {
+            throw new IllegalArgumentException("디폴트 구분자를 커스텀 구분자로 입력할 수 없습니다.");
+        }
     }
 }
